093b1feeed6c366677a8056f164501193fc25978
SeymourGx <seymourguado8000@gmail.com>
2021-12-03 21:36:22 -0700

    Heavy code cleanup

--- src/main/java/SeymourG/AFKOmega/AFKOmega.java
+++ src/main/java/SeymourG/AFKOmega/AFKOmega.java
@@ -16,6 +16,7 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.userdev.LaunchTesting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.ArrayList;
